e4ca58d099fc2e671b6c0126d6e15c89d7e70ab3
LAURA SANTOS BANDEIRA <25132517@dsn127m1246726.cfp127aluno.local>
2025-08-11 13:38:18 -0300

    consertando a função

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 const botaoTrocarImagem = document.getElementById('trocar-img')
 
 
+
 function trocarImagem() {
     const imagem = document.getElementById('imagem').value
-    document.documentElement.style.setProperty('--bg-img', `url('./img/${imagem}.jpg)')`)
+    document.documentElement.style.setProperty('--bg-img', `url('./img/${imagem}.jpg')`)
 }
 
 botaoTrocarImagem.addEventListener('click', trocarImagem)
